4b6008c3dd8dd6f1d58d70650e526a9534b0d67d
zhaojiecheng <zhaojiecheng@zhaojiechengs-MacBook-Pro.local>
2015-05-07 17:06:31 +0800

    md5 checksum

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo "deb http://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.lis
 sudo apt-get update
 sudo apt-get install sbt
 
-#install the spark to /usr/local/share
+#install the spark to $HOME/spark
 #see http://spark.apache.org
 #see http://spark.apache.org/downloads.html
 #see http://www.apache.org/dyn/closer.cgi/spark/spark-1.3.1/spark-1.3.1.tgz
@@ -17,6 +17,12 @@ mkdir /tmp/download
 cd /tmp/download
 
 wget http://apache.fayea.com/spark/spark-1.3.1/spark-1.3.1-bin-hadoop2.6.tgz
+#see http://www.apache.org/dist/spark/spark-1.3.1/spark-1.3.1-bin-hadoop2.6.tgz.md5
+if [[ $(md5sum /tmp/download/spark-1.3.1-bin-hadoop2.6.tgz) != "b71cda077405411ef9c450ec4b43b56b  /tmp/download/spark-1.3.1-bin-hadoop2.6.tgz" ]]
+	then echo "the md5 checksum doesn't match"
+	return 1
+fi
+
 mkdir -p $HOME/spark
 tar -xvf spark-1.3.1-bin-hadoop2.6.tgz -C $HOME/spark
 
