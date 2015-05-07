3ff952cf31907af301f2118f357421bec0a469b8
zhaojiecheng <zhaojiecheng@zhaojiechengs-MacBook-Pro.local>
2015-05-07 09:53:47 +0800

    remove scala

--- install.sh
+++ install.sh
@@ -6,20 +6,12 @@ sudo apt-get install sbt
 mkdir /tmp/download
 cd /tmp/download
 
-wget -T 10 http://downloads.typesafe.com/scala/2.10.5/scala-2.10.5.tgz?_ga=1.106928195.1176586385.1430617673 \
-  -O scala-2.10.5.tgz
-mkdir -p /usr/local/share/scala
-tar -xvf scala-2.10.5.tgz -C /usr/local/share/scala/
-
 wget http://apache.fayea.com/spark/spark-1.3.1/spark-1.3.1-bin-hadoop2.6.tgz
 mkdir -p /usr/local/share/spark
 tar -xvf spark-1.3.1-bin-hadoop2.6.tgz -C /usr/local/share/spark
 
-
-export SCALA_HOME=/usr/local/share/scala/scala-2.10.5
 export SPARK_HOME=/usr/local/share/spark/spark-1.3.1-bin-hadoop2.6
 
-chown -R root:root $SCALA_HOME
 chown -R root:root $SPARK_HOME
 mkdir $SPARK_HOME/logs
 chmod a+w $SPARK_HOME/logs
@@ -28,8 +20,7 @@ mkdir $SPARK_HOME/work
 chmod a+w $SPARK_HOME/work
 
 echo "
-export SCALA_HOME=$SCALA_HOME
 export SPARK_HOME=$SPARK_HOME
-export PATH=\$PATH:\$SCALA_HOME/bin:\$SPARK_HOME/bin:\$SPARK_HOME/sbin
+export PATH=\$PATH:\$SPARK_HOME/bin:\$SPARK_HOME/sbin
 " | tee -a /etc/profile
 
